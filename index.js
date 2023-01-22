3c34403c069ff6052d4f2229e13186cfdf788a3a
Mark Coats <alexco1234@gmail.com>
2023-01-21 19:27:23 -0600

    cat infront of screen, can't see what i'm typuibg. added methods to manipulate objects

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "name",
+    streetAddress: "streetAddress"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    console.log(employee);
+    console.log(newEmployee);
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
+deleteFromEmployeeByKey(employee, "name");
